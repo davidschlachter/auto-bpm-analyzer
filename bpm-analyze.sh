ac0da2374b2a145ae31509e56b2b15865c0f9446
David Schlachter <david+gh@schlachter.ca>
2017-01-17 18:09:01 -0500

    Check for audio files in the current directory before processing

--- bpm-analyze.sh
+++ bpm-analyze.sh
@@ -4,13 +4,25 @@
 # Usage: ./bpm-analyze.sh
 # With no arguments, searches for tracks in the current folder and attempts to tag them
 
-which bpm-tag || {
+which bpm-tag >/dev/null || {
 	echo "Please install 'bpm-tools' (http://www.pogo.org.uk/~mark/bpm-tools/)"
   exit 1
 	} # Check for bpm-tools
   
-which ffmpeg || {
+which ffmpeg >/dev/null || {
 	echo "Please install 'ffmpeg'"
   exit 1
 	} # Check for ffmpeg
   
+if (( ${BASH_VERSION%%.*} < 4 )); then
+  echo "'bash' version must be greater than or equal to 4"
+  exit 1
+fi
+
+# Enumerate audio files in the current directory
+# (verify that there are audio files to process)
+find ./ | grep '\.mp3$\|\.mp4$\|\.m4a$\|\.wav$\|\.pcm$\|\.aif$\|\.aiff$\|\.aac$\|\.ogg$\|\.wma$\|\.flac$\|\.alac$' > /dev/null 2>&1
+if [ "$?" != "0" ]; then
+  echo "The current directory must contain the audio files to process."
+  exit 1
+fi
